2652662853182c66e51f9708f45ee2e6115e6337
Shivaraj <02Shivaraj@gmail.com>
2025-06-03 19:11:28 +0530

    app.js file modified

--- backend/app.js
+++ backend/app.js
@@ -80,9 +80,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
-module.exports = app;
-
 // Start server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
